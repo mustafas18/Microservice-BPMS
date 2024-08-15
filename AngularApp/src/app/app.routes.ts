dec3f606505bb19ffb9b721310c4717e23535dbb
M <M@DESKTOP-0QJT7IB>
2024-08-16 00:27:59 +0330

    delete angular app

--- AngularApp/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-
-export const routes: Routes = [
-    {path:"", component: LoginComponent},
-    {path:"auth/login", component: LoginComponent}
-];
